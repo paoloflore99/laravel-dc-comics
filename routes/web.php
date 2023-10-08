f011747b42019ab9b722e75a01b7a800b9520a9b
paoloflore99 <paoloflore937@gmail.com>
2023-10-08 11:33:40 +0200

    rotte completate

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,22 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', [HomeController::class, "index"])->name("home");
+
+//create 
+route::get('/comics/create', [HomeController::class, "create"])->name("comics.create");
+
+//store
+route::post('/comics', [HomeController::class, "store"])->name("comics.store");
+
+route::get('/comics', [HomeController::class, "index"])->name("comics.home");
+
+route::get('/comics/{id}', [HomeController::class, "show"])->name("comics.show");
+
+//edit
+route::get('/comics/{id}/edit', [HomeController::class, "edit"])->name("comics.edit");
+
+//updtade
+route::put('/comics/{id}', [HomeController::class, "updtade"])->name("comics.updtade");
+
+//destroy
+route::delete('/comics/{id}', [HomeController::class, "destroy"])->name("comics.destroy");
